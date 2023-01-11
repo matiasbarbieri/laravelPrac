12231554a7624016b1cd3186e74fd816753d8724
Matias Barbieri <matias.nbarbieri@gmail.com>
2023-01-11 12:32:30 -0300

    agregando portfolio controller

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,16 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortfolioController;
 
-$portfolio = [
-    ['title' => 'Proyecto #1'],
-    ['title' => 'Proyecto #2'],
-    ['title' => 'Proyecto #3'],
-    ['title' => 'Proyecto #4'],
-];
+
+
+
 
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
+Route::get('/portfolio', PortfolioController::class)->name('portfolio');
+
+
 Route::view('/contact', 'contact')->name('contact');
 
 
