177b3ad4ceceef4957a1651cc92f4baca4b5dda9
Matias Barbieri <matias.nbarbieri@gmail.com>
2023-01-17 16:58:10 -0300

    configurando envio de mails

--- app/Http/Controllers/Messagescontroller.php
+++ app/Http/Controllers/Messagescontroller.php
@@ -3,18 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\MessageReceived;
 
 class Messagescontroller extends Controller
 {
     public function store()
     {
-        request()->validate([
+        $message = request()->validate([
             'name' => 'required',
             'email' => 'required|email',
             'subject' => 'required',
             'content' => 'required|min:3'
         ]);
 
-        return 'datos validados';
+        //enviar el email
+        //Mail::to('matias.nbarbieri@gmail.com')->queue(new MessageReceived($message));
+
+        //provisorio para ver el mail enviado en el navegador
+        return new MessageReceived($message);
+
+        return 'Mensaje Enviado';
     }
 }
---
+++ app/Mail/MessageReceived.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class MessageReceived extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $subject = 'Mensaje recibido';
+
+    public $msg;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($msg)
+    {
+        $this->msg = $msg;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Message Received',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'emails.message-received',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
