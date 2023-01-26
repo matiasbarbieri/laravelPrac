d6ade93c9c1afc0929bc43a51879f3fd13a4c8e0
Matias Barbieri <matias.nbarbieri@gmail.com>
2023-01-25 22:53:17 -0300

    bootstrap

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,8 +3,9 @@
 <head>
 
     <title>@yield('title', 'aprendible')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite(['resources/sass/app.scss', 'resources/css/app.css',  'resources/js/app.js'])
-
     <link rel="stylesheet" href="{{ asset('resources/css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('resources/js/app.js') }}">
     <link rel="stylesheet" href="{{ asset('resources/sass/app.scss') }}">
@@ -16,8 +17,17 @@
     </style>
 </head>
 <body>
-    @include('partials.nav')
-    @include('partials.session-status')
-    @yield('content')
+    <div id="app" class="d-flex flex-column h-screen justify-content-between">
+        <header>
+            @include('partials.nav')
+            @include('partials.session-status')
+        </header>
+        <main>
+            @yield('content')
+        </main>
+        <footer class="bg-white text-center text-black-50 py-3 shadow">
+            {{ config('app.name') }} | Copyright @ {{ date('Y') }}
+        </footer>
+    </div>
 </body>
 </html>
